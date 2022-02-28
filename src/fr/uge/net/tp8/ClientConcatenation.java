bc528bc401c6f279721ca1a6bf21bb41bed25e23
selgueti <sofian.elguetibi@gmail.com>
2022-02-28 18:02:04 +0100

    work on tp8 ex3 (ClientConcatenation) clear

--- src/fr/uge/net/tp8/ClientConcatenation.java
+++ src/fr/uge/net/tp8/ClientConcatenation.java
@@ -1,94 +1,120 @@
 package fr.uge.net.tp8;
 
-import fr.uge.net.tp5.LongSumPacket;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class ClientConcatenation {
 
     public static final Logger logger = Logger.getLogger(ClientLongSum.class.getName());
-
-    /*
-     * Lis le clavier jusqua recontrer la chaine vide ""
-     * Renvoie la liste des chaines sans la dernière chaine vide.
-     */
-    List<String> readClavier(){
-        return null;
-    }
-
-    /*
-     * Envoie la requête et attend la réponse si le serveur n'a pas fermé la connexion
-     * Renvoie Optional.empty() si le serveur à fermé la connexion, sinon renvoie la concaténation.
-     */
-    private static Optional<String> requestConcatenationForList(SocketChannel sc, List<String> list) throws IOException{
-        return Optional.empty();
-    }
-
+    public static final Charset UTF8 = StandardCharsets.UTF_8;
 
     /**
-     * Write all the longs in list in BigEndian on the server and read the long sent
-     * by the server and returns it
-     *
-     * returns Optional.empty if the protocol is not followed by the server but no
-     * IOException is thrown
-     *
-     * @param sc
-     * @param list
-     * @return
-     * @throws IOException
+     * Reads keyboard until reading the empty string
+     * @return a list of strings without the last empty string
      */
-    private static Optional<Long> requestConcatenationForList(SocketChannel sc, List<Long> list) throws IOException {
-        ByteBuffer sendBuffer = ByteBuffer.allocate(Integer.BYTES + list.size() * Long.BYTES);
-        var sizeBuffer = Long.BYTES;
-        ByteBuffer receiveBuffer = ByteBuffer.allocate(sizeBuffer);
+    private static List<String> readClavier() {
+        Scanner scanner = new Scanner(System.in);
+        var stringList = new ArrayList<String>();
+        while(scanner.hasNext()){
+            var string = scanner.next();
+            if(string.equals("")){
+                break;
+            }
+//            System.out.println(string);
+            stringList.add(string);
+        }
+        return stringList;
+    }
+
+    /**
+     * Sends request
+     * @param sc - SocketChannel which represents the connexion
+     * @param list - List<String> list of string to be concatenated
+     * @throws IOException - If some I/O error occurs
+     * */
+    private static int sendRequest(SocketChannel sc, List<String> list) throws IOException {
+        int sizeBuffer = (list.size() + 1) * Integer.BYTES;
+        var bufferList = list.stream().map(UTF8::encode).toList();
+        for (var bb: bufferList) {
+            sizeBuffer += bb.remaining();
+        }
+        ByteBuffer sendBuffer = ByteBuffer.allocate(sizeBuffer);
         sendBuffer.putInt(list.size());
-        list.forEach(sendBuffer::putLong);
+        bufferList.forEach(bb -> {
+            sendBuffer.putInt(bb.remaining());
+            sendBuffer.put(bb);
+        });
         sendBuffer.flip();
         sc.write(sendBuffer);
-
-        if(!readFully(sc, receiveBuffer)){
-            return Optional.empty();
-        }
-
-        receiveBuffer.flip();
-        return Optional.of(receiveBuffer.getLong());
+        return sizeBuffer - (list.size() + 1) * Integer.BYTES;
     }
 
     /**
-     * Fill the workspace of the Bytebuffer with bytes read from sc.
+     * Sends request and wait the response if server hasn't closed connexion
+     * @param sc - SocketChannel which represents the connexion
+     * @param list - List<String> list of string to be concatenated
+     * @return Options.empty() if the server has closed connexion, otherwise returns the concatenation
+     * @throws IOException - If some I/O error occurs
+     */
+    private static Optional<String> requestConcatenationForList(SocketChannel sc, List<String> list) throws IOException{
+        var sizeBuffer = sendRequest(sc, list);
+        var receiveBuffer = ByteBuffer.allocate(Integer.BYTES);
+
+        if(!readFully(sc, receiveBuffer)){
+            logger.info("Connexion closed before processing");
+            return Optional.empty();
+        }
+        receiveBuffer.flip();
+        sizeBuffer = receiveBuffer.getInt();
+        receiveBuffer = ByteBuffer.allocate(sizeBuffer);
+        if(!readFully(sc, receiveBuffer)){
+            logger.info("Connexion closed before processing");
+            return Optional.empty();
+        }
+        receiveBuffer.flip();
+        var msg = UTF8.decode(receiveBuffer).toString();
+        System.out.println(msg);
+        return Optional.of(msg);
+    }
+
+    /**
+     * Fill the workspace with the Bytebuffer with bytes read from sc.
      *
-     * @param sc
-     * @param buffer
+     * @param sc - SocketChannel which represents the connexion
+     * @param buffer - Buffer to fill
      * @return false if read returned -1 at some point and true otherwise
-     * @throws IOException
+     * @throws IOException – If some I/O error occurs
      */
     static boolean readFully(SocketChannel sc, ByteBuffer buffer) throws IOException {
-        while(-1 != sc.read(buffer) && buffer.hasRemaining());
-        return !buffer.hasRemaining();
+        while(-1 != sc.read(buffer)){
+            if(!buffer.hasRemaining()){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) throws IOException {
         var server = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
         try (var sc = SocketChannel.open(server)) {
-            for (var i = 0; i < 5; i++) {
-                var list = randomLongList(50);
 
-                var sum = requestSumForList(sc, list);
-                if (!sum.isPresent()) {
-                    logger.warning("Connection with server lost.");
-                    return;
-                }
-                if (!checkSum(list, sum.get())) {
-                    logger.warning("Oups! Something wrong happened!");
-                }
+            var stringsList = readClavier();
+            var response = requestConcatenationForList(sc, stringsList);
+            if(response.isEmpty()){
+                logger.warning("Connection with server lost.");
+            }else{
+                logger.info("Everything seems ok");
+                System.out.println(response.get());
             }
-            logger.info("Everything seems ok");
         }
     }
 }
